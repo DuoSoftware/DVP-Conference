7e85c3ab92ebdb7c689b175dad950706fb00e9f8
Pawan Sasanka <pawan@duosoftware.com>
2015-07-22 17:38:05 +0530

    Config updated

---
+++ config/default.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "DB": {
+    "Type":"postgres",
+    "User":"postgres",
+    "Password":"DuoS123",
+    "Port":5432,
+    "Host":"127.0.0.1",
+    "Database":"dvpdb"
+  },
+  "Redis":
+  {
+    "ip": "192.168.3.200",
+    "port": 6379
+
+  },
+
+  "Host":
+  {
+    "domain": "localhost",
+    "port": 8085,
+    "version":"6.0"
+  }
+};
