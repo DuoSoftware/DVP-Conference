bd111406ceb829a3a2c4bc3eddbffa553482e270
Pawan Sasanka <pawan@duosoftware.com>
2016-04-05 16:17:12 +0530

    config updated

--- config/default.js
+++ config/default.js
@@ -10,15 +10,15 @@ module.exports = {
   "Redis":
   {
     "ip": "45.55.142.207",
-    "port": 6379,
+    "port": 6389,
     "password":"DuoS123"
 
   },
   "Security":
   {
     "ip" : "45.55.142.207",
-    "port": 6379,
-    "user": "duo",
+    "port": 6389,
+    "user": "",
     "password": "DuoS123"
   },
 
